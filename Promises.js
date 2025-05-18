e0ba2cd4bf005e8d9e3e08b438c6bb9d9ab7e937
NarayanPhukan <narayanphukan30@gmail.com>
2025-05-18 18:12:17 +0530

    Starting Backend

---
+++ Promises.js
@@ -0,0 +1,20 @@
+const simranpromise = new Promise((resolve, reject)=>{
+
+    let parentsDecision = false
+
+    if(parentsDecision){
+        resolve("Accepted")
+    }
+    else {
+        reject("Rejected")
+    }
+})
+
+simranpromise.then((msg)=>{
+    console.log("Simran message : ", msg)
+    console.log("Let's book the wedding venue")
+}).catch((msg)=>{
+    console.log("Simran message : ", msg)
+    console.log("Tinder kaha ho yaar")
+})
+
